56de6c794cf3c9448ac48048314dc32891ba4be3
tkarshi <tkarshi2000@gmail.com>
2024-07-12 06:43:35 +0530

    Automatic Conversion of JS

---
+++ index.js
@@ -0,0 +1,28 @@
+let output = document.getElementById("outputScreen");
+
+// output.innerHTML = "5" - 2;
+
+// output.innerHTML = "5" * 2;
+
+output.innerHTML = "5" % 2;
+
+// output.innerHTML = "5" / 2;
+
+// // output.innerHTML = 8.7 + 8;
+
+// // output.innerHTML = null == undefined;
+
+// // output.innerHTML = "null" == undefined;
+
+// output.innerHTML = "" == false;
+
+// function check() {
+//   let myInput = document.getElementById("myInput");
+//   //   alert(myInput.value);
+
+//   if (myInput.value == "") {
+//     alert("Please enter the value");
+//   } else {
+//     alert(myInput.value);
+//   }
+// }
